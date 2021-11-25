86a1c66a59e9f72c5b04264976284f2aa2dcec3b
Walid Wazini <wazini25@gmail.com>
2021-11-25 17:13:00 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ActiveElement from './components/ActiveElement.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('active-element', ActiveElement)
+app.component('hello-world')
+
+app.mount('#app')
